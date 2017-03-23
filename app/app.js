9eed555a201b33c64f0308d3600c42b87757cbe5
rivaille-c <rivaille-c@mgdis.fr>
2017-03-23 14:19:23 +0100

    refactor: app is a stateless component connected to redux

--- app/app.js
+++ app/app.js
@@ -1,47 +1,38 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import LinkedKeyboard from './components/containers/linked-keyboard.component'
 import LinkedChord from './components/containers/linked-chord.component'
 
-export default class HarmonItApp extends React.Component {
-
-  constructor(props, context) {
-    super(props, context)
-
-    this.state = {}
-    this.context.store.subscribe(() => {
-      this.setState({
-        baseSelected: this.context.store.getState().chord.base !== undefined
-      })
-    })
+let HarmonitApp = (props) => {
+  const style = {
+    keyboard: {
+      height: '50%',
+      backgroundColor: '#96e6f8'
+    },
+    chord: {
+      height: '50%',
+      backgroundColor: '#5151a1'
+    },
+    width: '100%',
+    height: '100%'
   }
 
-  render() {
-    const style = {
-      keyboard: {
-        height: '50%',
-        backgroundColor: '#96e6f8'
-      },
-      chord: {
-        height: '50%',
-        backgroundColor: '#5151a1'
-      },
-      width: '100%',
-      height: '100%'
-    }
-
-    return (
-      <div style={style}>
-        <div style={style.keyboard}>
-          <LinkedKeyboard />
-        </div>
-        <div style={style.chord}>
-          {this.state.baseSelected ? <LinkedChord/> : <div></div>}
-        </div>
+  return (
+    <div style={style}>
+      <div style={style.keyboard}>
+        <LinkedKeyboard />
       </div>
-    )
-  }
+      <div style={style.chord}>
+        {props.baseSelected ? <LinkedChord/> : <div></div>}
+      </div>
+    </div>
+  )
 }
 
-HarmonItApp.contextTypes = {
-  store: React.PropTypes.object.isRequired
-}
+HarmonitApp = connect((state) => {
+  return {
+    baseSelected: state.chord.base !== undefined
+  }
+}, {})(HarmonitApp)
+
+export default HarmonitApp
